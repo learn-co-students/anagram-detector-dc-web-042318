439e1807f4d691ab8e60ec0bb998bf70525098cc
Adam Cohn <adamcohn88@gmail.com>
2018-04-28 18:53:11 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,37 +1,22 @@
-require 'pry'
 class Anagram
-  attr_accessor :word, :match, :word_hash
+  attr_accessor :word
+  attr_writer :match
 
   def initialize(word)
     @word = word
   end
-  
-  def count_word
-    word_hash = Hash.new(0)
-    self.word.chars.each{ |w| word_hash[w] += 1}
-    word_hash
+
+  def sort_word
+    self.word.chars.sort
   end
-  
-  def parse_matches(match)
-    word_counts = []
-    match.each do |w|
-      match_hash = Hash.new(0)
-      w.chars.each{ |l| match_hash[l] += 1 }
-      word_counts << match_hash
-    end
-    word_counts
+
+  def sort_matches(words)
+    words.map { |w| w.chars.sort }
   end
-  
+
   def match(match)
     answer = []
-    parse_matches(match).each do |ml, mc|
-      count_word.each do |wl, wc|
-        #binding.pry
-        if ml == wl && mc <= wc
-          answer << ml.keys.join
-        end
-      end
-    end
-    answer
+    sort_matches(match).each_with_index { |w, i| answer << match[i] if w == sort_word }
+    answer.flatten
   end
 end
